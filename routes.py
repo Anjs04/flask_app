7ca41f4d849ed83275d7ae309c4d41e04da6756d
Anjali <anjali.sharma1@cba.com.au>
2018-12-28 19:40:29 +1100

    Add about page + ssl

--- routes.py
+++ routes.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('certs/server.crt', 'certs/server.key')
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,5 +15,5 @@ def about():
   return render_template("about.html")
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='127.0.0.1',port='5000', debug = True, ssl_context=context)
   
